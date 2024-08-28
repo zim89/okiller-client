4c56c84064a02d0807e1ad92d3e91f09ae622512
Demirov Vlad <demirov.vlad@gmail.com>
2024-08-27 22:18:46 -0800

    Auh0 configured and simple authentication added

--- src/components/Header/ui/NavToolbar.tsx
+++ src/components/Header/ui/NavToolbar.tsx
@@ -1,11 +1,11 @@
-import { AccountButton } from './AccountButton'
+import { ProfileDropdown } from '@/components/Header/ui/ProfileMenu.tsx'
 import { CartButton } from './CartButton'
 import { FavoriteButton } from './FavoriteButton'
 
 export const NavToolbar = () => {
   return (
     <ul className='hidden xl:flex xl:items-center xl:gap-5'>
-      <AccountButton />
+      <ProfileDropdown />
       <FavoriteButton />
       <CartButton />
     </ul>
